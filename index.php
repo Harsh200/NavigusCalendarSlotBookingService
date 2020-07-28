1a396e24843b218728959505b85e639a524437aa
Harsh200 <harsh.saxena_ccv17@gla.ac.in>
2020-07-28 17:19:59 +0530

    providing link for frontpage and icon

---
+++ index.php
@@ -0,0 +1,10 @@
+!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="css/frontpage.css">
+<link rel="icon" href="images/kgtdevlogoresized.jpg" type="image/x-icon">
+<script>
+
+</script>
+</head>
