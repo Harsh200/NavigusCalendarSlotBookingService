877847e606314a17bca9b343c601d920c278ce74
Harsh200 <harsh.saxena_ccv17@gla.ac.in>
2020-07-28 17:53:13 +0530

    call this name to execute

--- registration_page.php
+++ registration_page.php
@@ -6,3 +6,27 @@ if(isset($_SESSION["username"])){
     exit();
 }
 ?><?php
+if(isset($_POST["usernamecheck"])){
+  include_once("php_include/connection1.php");
+  $isset = $_POST["usernamecheck"];
+  $username = preg_replace('#[^a-z0-9]#i', '', $_POST['usernamecheck']);
+  $sql = "SELECT member_id FROM members WHERE username='$username' LIMIT 1";
+    $query = mysqli_query($db_conx, $sql);
+    $uname_check = mysqli_num_rows($query);
+    if (strlen($username) < 3 || strlen($username) > 16) {
+      echo '<strong style="color:#F00;">3 - 16 characters please</strong>';
+      exit();
+    }
+  if (is_numeric($username[0])) {
+      echo '<strong style="color:#F00;">Usernames must begin with a letter</strong>';
+      exit();
+    }
+    if ($uname_check < 1) {
+      echo '<strong style="color:#009900;">The username ' . $username . ' is OK</strong>';
+      exit();
+    } else {
+      echo '<strong style="color:#F00;">The username ' . $username . ' is taken</strong>';
+      exit();
+    }
+}
+?><?php
